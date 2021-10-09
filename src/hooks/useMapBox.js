ea33fee55d18b7588b3198d1812e3ff716bf1393
Luis Carrion <lcarrionalvarez@gmail.com>
2021-10-08 22:07:24 -0500

    useMapBox added

---
+++ src/hooks/useMapBox.js
@@ -0,0 +1,83 @@
+import mapboxgl from 'mapbox-gl';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {v4} from 'uuid';
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3lhbi1jb2RlIiwiYSI6ImNrdWhvejByMjAwaHAyb3A4ODNleDlndXQifQ.52wKLh1X6ZTsqiGLuOWQuQ';
+
+export const useMapBox = (puntoInicial) => {
+
+  const mapaDiv = useRef();
+  const setRefDivMap = useCallback((node) => { // pasar el nodo en el elemento en el que el mapa necesita renderizarse
+    mapaDiv.current = node
+  },[])
+
+  // Referencia a los marcadores
+  const marcadores = useRef({});
+
+
+  //Mapa y coords
+  const mapa = useRef();  //Ref en vez de useState
+  const [coords, setCoords] = useState(puntoInicial)
+
+  //Funcion para agregar marcadores
+  const agregarMarcador = useCallback((ev) => {    
+      const {lng, lat} = ev.lngLat;
+      const marker = new mapboxgl.Marker();
+      marker.id = v4(); // TODO si el marker tiene id
+
+      marker
+        .setLngLat([lng, lat])
+        .addTo( mapa.current )
+        .setDraggable(true)
+
+      marcadores.current[marker.id] = marker; // Whyyyy????
+
+      //Escuchar movimientos del marcador
+      marker.on('drag', ({target}) => {
+        const {id} = target;
+        const {lng, lat} = target.getLngLat();
+        // TODO: Emitir los cambios del marcador
+      })
+
+  },[])
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapaDiv.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ puntoInicial.lng, puntoInicial.lat ],
+      zoom: puntoInicial.zoom
+      });
+    mapa.current = map;
+  }, [puntoInicial])
+  
+  // Detectar Moviemiento del mapa
+  useEffect(() => {
+    mapa.current?.on('move', () => {
+      const {lng, lat} = mapa.current.getCenter();
+      setCoords({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: mapa.current.getZoom().toFixed(2)
+      })
+    }) 
+    return () => {
+      mapa.current?.off('move')
+    }
+  }, [mapa])
+
+  // agregar marcadores cuando haga click
+  useEffect(() => {
+    mapa.current?.on('click', agregarMarcador)
+    return () => {
+      mapa.current?.off('click')
+    }
+  },[agregarMarcador])
+
+  return {
+    agregarMarcador,
+    coords,
+    setRefDivMap,
+  }
+}
--- src/pages/MapaPage.js
+++ src/pages/MapaPage.js
@@ -1,44 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiY3lhbi1jb2RlIiwiYSI6ImNrdWhvejByMjAwaHAyb3A4ODNleDlndXQifQ.52wKLh1X6ZTsqiGLuOWQuQ';
+import React from 'react'
+import { useMapBox } from '../hooks/useMapBox';
 
 const puntoInicial = {
-  lng: 5,
-  lat: 34,
-  zoom: 2
+  lng: -122.4725,
+  lat: 37.801,
+  zoom: 13.5
 }
 
 export const MapaPage = () => {
 
-  const mapaDiv = useRef();
-  const [mapa, setMapa] = useState(null)
-  const [coords, setCoords] = useState(puntoInicial)
-
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapaDiv.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [ puntoInicial.lng, puntoInicial.lat ],
-      zoom: puntoInicial.zoom
-      });
-    setMapa(map)
-  }, [])
+  const {setRefDivMap, coords, agregarMarcador} = useMapBox(puntoInicial) //Estudiar este Hook
   
-  // Detectar Moviemiento del mapa
-  useEffect(() => {
-    mapa?.on('move', () => {
-      const {lng, lat} = mapa.getCenter();
-      setCoords({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: mapa.getZoom().toFixed(2)
-      })
-    }) 
-    return () => {
-      mapa?.off('move')
-    }
-  }, [mapa])
 
   return (
     <>
@@ -46,7 +18,7 @@ export const MapaPage = () => {
         lng: {coords.lng} | lat: {coords.lat} | zoom: {coords.zoom}
       </div>
       <div
-        ref={mapaDiv} 
+        ref={setRefDivMap} 
         className="mapContainer"
       />
     </>
